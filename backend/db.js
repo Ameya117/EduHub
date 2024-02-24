d1d7a9bfcb697c89321995f7d5ffcaa138deabc3
Ameya <ameya.varade117@gmail.com>
2024-02-24 13:02:36 +0530

    backend folder deleted

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = import.meta.VITE_mongoURI;
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI).then(()=>console.log("Connected to database")).catch((e)=>console.log(e.message))
-}
-
-module.exports = connectToMongo;
