1a910c2b6c87db40c093eff4a87c5affc579efc1
Ameya117 <114793226+Ameya117@users.noreply.github.com>
2024-03-17 15:07:57 +0530

    Update News.jsx
    commented news component because of API issue

--- src/components/HomePage/News.jsx
+++ src/components/HomePage/News.jsx
@@ -31,38 +31,38 @@ const News = () => {
   },[])
 
   return (
-    <div className="w-full mt-8 relative -top-16 md:-top-24">
-      {/* <h1 className="text-3xl text-center font-semibold mb-3">Latest News</h1> */}
-      {news ? (
-        <Carousel
-          opts={{
-            align: "start",
-          }}
-          className="w-[75%] mx-auto"
-        >
-          <CarouselContent >
-            {news.map((item) => {
-              return (
-                <div key={item.url} className="w-full">
-                  <CarouselItem className="md:basis-1/2 mx-2">
-                    <NewsCard
-                      title={item.title}
-                      urlToImg={item.urlToImage}
-                      url={item.url}
-                      author={item.author}
-                    />
-                  </CarouselItem>
-                </div>
-              );
-            })}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      ) : (
-        <div className="text-center mx-auto grid place-content-center">{<Loading/>}</div>
-      )}
-    </div>
+    // <div className="w-full mt-8 relative -top-16 md:-top-24">
+    //   {/* <h1 className="text-3xl text-center font-semibold mb-3">Latest News</h1> */}
+    //   {news ? (
+    //     <Carousel
+    //       opts={{
+    //         align: "start",
+    //       }}
+    //       className="w-[75%] mx-auto"
+    //     >
+    //       <CarouselContent >
+    //         {news.map((item) => {
+    //           return (
+    //             <div key={item.url} className="w-full">
+    //               <CarouselItem className="md:basis-1/2 mx-2">
+    //                 <NewsCard
+    //                   title={item.title}
+    //                   urlToImg={item.urlToImage}
+    //                   url={item.url}
+    //                   author={item.author}
+    //                 />
+    //               </CarouselItem>
+    //             </div>
+    //           );
+    //         })}
+    //       </CarouselContent>
+    //       <CarouselPrevious />
+    //       <CarouselNext />
+    //     </Carousel>
+    //   ) : (
+    //     <div className="text-center mx-auto grid place-content-center">{<Loading/>}</div>
+    //   )}
+    // </div>
   );
 };
 
